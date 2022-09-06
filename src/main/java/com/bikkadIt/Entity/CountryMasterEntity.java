2b2f191cf6ccad21c6284411899dc50bb03a3fc2
LatikaJadhav94 <latikaj189@gmail.com>
2022-09-06 18:42:28 +0530

    UMA-05: Create entity class for given 4 tables

--- src/main/java/com/bikkadIt/Entity/CountryMasterEntity.java
+++ src/main/java/com/bikkadIt/Entity/CountryMasterEntity.java
@@ -1,8 +1,25 @@
 package com.bikkadIt.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
+@Table(name="COUNTRY_MASTER")
+@Data
 public class CountryMasterEntity {
+	
+	@Id
+	@Column(name= "COUNTRY_ID")
+	private Integer countryId;
+	
+	@Column(name="COUNTRY_Name")
+	private String countryName;
+	
+	@Column(name="COUNTRY_CODE")
+	private String countryCode;
 
 }
--- src/main/java/com/bikkadIt/Entity/StateMasterEntity.java
+++ src/main/java/com/bikkadIt/Entity/StateMasterEntity.java
@@ -1,8 +1,24 @@
 package com.bikkadIt.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
+@Table(name="STATE_MASTER")
+@Data
 public class StateMasterEntity {
-
+	
+	@Id
+	@Column(name="STATE_ID")
+	private Integer stateId;
+	
+	@Column(name="STATE_NAME")
+	private String stateName;
+	
+	@Column(name="COUNTRY_ID")
+	private Integer countryId;
 }
